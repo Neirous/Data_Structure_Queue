24335aa960de8f5b1ca6803ccdd8c84b52bd2fcc
Neirous <113926999+Neirous@users.noreply.github.com>
2023-10-11 18:46:00 +0800

    Add files via upload

--- Queue01.c
+++ Queue01.c
@@ -23,7 +23,7 @@ int isQueueEmpty(Queue* q) {
 }
 
 // 入队
-void enqueue(Queue* q, char data) {
+void enQueue(Queue* q, char data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->next = NULL;
@@ -36,7 +36,7 @@ void enqueue(Queue* q, char data) {
 }
 
 // 出队
-char dequeue(Queue* q) {
+char deQueue(Queue* q) {
     if (isQueueEmpty(q)) {
         printf("Queue is empty.\n");
         return '\0';
@@ -64,22 +64,34 @@ void printQueue(Queue* q) {
 
 int main() {
     Queue q;
-    initQueue(&q);
-
-    // 模拟排队
-    enqueue(&q, 'A');
-    enqueue(&q, 'B');
-    enqueue(&q, 'C');
-
-    // 打印队列中的元素
-    printQueue(&q);
-
-    // 模拟就诊
-    dequeue(&q);
-
-    // 打印队列中的元素
-    printQueue(&q);
-
+    int choice;
+    char name;
+	fflush(stdin);
+	initQueue(&q);
+    do {
+		printf("0.stopwork 1.queue 2.seedoctor 3.seequeue please select:");
+		scanf("%d", &choice);
+		fflush(stdin);
+		switch (choice) {
+		case 1:
+			printf("请输入要录入的人的姓名：");
+			scanf("%c",&name);
+			enQueue(&q,name);
+			fflush(stdin);
+			break;
+		case 2:
+			name = deQueue(&q);
+			if(name != '\0')
+				printf("patient:%c see doctor!\n", name);
+			break;
+		case 3:
+			printQueue(&q);
+			break;
+		case 0:
+			exit(-1);
+			break;
+		}
+	} while (choice != 0);
     return 0;
 }
 
